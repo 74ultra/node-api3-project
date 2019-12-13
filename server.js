ffa82ae07b84d5d851937a3fb391639bb1ad68ae
74ultra <r.barnhill@protonmail.com>
2019-12-13 15:59:08 -0800

    MVP met

--- server.js
+++ server.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json());
+server.use(logger)
+
+const userRouter = require('./users/userRouter.js')
+server.use('/api/users', userRouter)
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`Method: ${req.method} | URL: ${req.url} | Timestamp: ${new Date()}`)
+  next();
+}
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,143 @@
 const express = require('express');
+const User = require('./userDb.js');
+const Posts = require('../posts/postDb.js')
+
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-  // do your magic!
+router.post('/', validateUser, (req, res) => {
+  const { name } = req.body;
+  console.log(req.body)
+  User.insert({name})
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "Error inserting user"})
+    })
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+  const { id: user_id } = req.params;
+  const { text } = req.body;
+  Posts.insert({ user_id, text })
+    .then(post => {
+      res.status(201).json(post)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "Error inserting new post"})
+    })
 });
 
 router.get('/', (req, res) => {
-  // do your magic!
+  User.get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "Error retrieving users"})
+    })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(goober)
+  // const { id } = req.params;
+  // User.getById(id)
+  //   .then(user => {
+  //     res.status(200).json(user)
+  //   })
+  //   .catch(err => {
+  //     console.log(err)
+  //     res.status(500).json({ error: "Error retrieving user."})
+  //   })
 });
 
-router.get('/:id/posts', (req, res) => {
-  // do your magic!
+router.get('/:id/posts', validateUserId, (req, res) => {
+  const { id } = req.params;
+  User.getUserPosts(id)
+    .then(posts => res.status(200).json(posts))
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: "There was an error retrieve the user's posts"})
+    })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validateUserId, (req, res) => {
+  // const { id } = req.params;
+  const { id } = goober;
+  User.remove(id)
+    .then(() => {
+      res.status(200).end()
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({error: "There was a problem deleting the user"})
+    })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validateUserId, (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+        User.update(id, { name })
+          .then(updated => {
+            if(updated){
+              User.getById(id)
+                .then(user => {
+                  res.status(200).json(user)
+                })
+                .catch(err => {
+                  console.log(err)
+                  res.status(500).json({ error: "Error getting user"})
+                })
+            } else {
+              res.status(500).json({error: "There was a problem updating that user"})
+            }
+          })
+          .catch(err => {
+            console.log(err)
+            res.status(500).json({ error: "Error updating user." })
+          })
 });
+    
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+  User.getById(id)
+    .then(user => {
+      if(user){
+        goober = user;
+        next();
+      } else {
+        res.status(404).json({ error: `A user with the id of ${id} does not exist`})
+      }
+    })
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const { name } = req.body;
+  if(!name){
+    res.status(400).json({ message: "A name is required for a new user" })
+  } else if (typeof name !== 'string') {
+    res.status(400).json({ message: "Name must be a string" })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const { text } = req.body;
+  if(!req.body){
+    return res.status(400).json({ error: "Missing post data"})
+  } else if (!text) {
+    return res.status(400).json({ error: "Missing required text field"})
+  } else {
+    next()
+  }
 }
 
 module.exports = router;
